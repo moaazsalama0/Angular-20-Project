16bcba259e28312a359690afa8a42dcea7379696
unknown <moaazsalama.2005@gmail.com>
2025-08-29 23:37:20 +0300

    final inshallah

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { HomeComponent } from './home/home.component';
 import { CartComponent } from './components/cart/cart.component';
 import { CategoriesComponent } from './categories/categories.component';
 import { CheckoutComponent } from './checkout/checkout.component';
+import { ContactComponent } from './contact/contact.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'cart', component: CartComponent },
   { path: 'categories', component: CategoriesComponent },
   {path: 'checkout', component: CheckoutComponent},
+  { path: 'contact', component: ContactComponent },
   // { path: '**', redirectTo: '' },
 ];
